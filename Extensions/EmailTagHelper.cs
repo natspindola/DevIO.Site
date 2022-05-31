ccc47d834e984a6d74786b3a1c73546533ab06d4
Perk <natspindola@hotmail.com>
2022-05-30 21:05:47 -0300

    domínio email

--- Extensions/EmailTagHelper.cs
+++ Extensions/EmailTagHelper.cs
@@ -8,11 +8,13 @@ namespace DevIO.Site.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "desenvolverdor.io";
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "desenvolvedor.io";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto" + target);
             output.Content.SetContent(target);
         }
