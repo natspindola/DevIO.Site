86e139786795417464c5b2133c3e2c1296925196
Perk <natspindola@hotmail.com>
2022-06-02 18:25:59 -0300

    adicionando suporte do DbContext

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,14 @@ namespace DevIO.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RazorViewEngineOptions>(options =>
@@ -26,7 +36,8 @@ namespace DevIO.Site
                 options.AreaViewLocationFormats.Add(item: "/Views/Shared/{0}.cshtml");
             });
 
-            services.AddDbContext<MeuDbContext>();
+            services.AddDbContext<MeuDbContext>(optionsAction: options =>
+            options.UseSqlServer(Configuration.GetConnectionString(name: "MeuDbContext")));
 
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
 
