7986acac0c612c31467f8cd05cf89dd4eba6f561
Perk <natspindola@hotmail.com>
2022-06-02 18:41:45 -0300

    crud / salvando no BD

---
+++ Controllers/TesteCrudController.cs
@@ -0,0 +1,35 @@
+﻿using DevIO.Site.Data;
+using DevIO.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Natalia",
+                DataNascimento = DateTime.Now,
+                Email = "natspindola@hotmail.com"
+            };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            return View();
+        }
+    }
+}
--- Data/MeuDbContext.cs
+++ Data/MeuDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevIO.Site.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace DevIO.Site.Data
         {
 
         }
+
+        public DbSet<Aluno> Alunos { get; set; }
     }
 }
