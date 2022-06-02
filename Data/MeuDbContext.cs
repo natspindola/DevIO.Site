e84ecd07f683b646cc4a8ce3bae3e79636bec8d9
Perk <natspindola@hotmail.com>
2022-06-02 18:27:46 -0300

    config DbContext

--- Data/MeuDbContext.cs
+++ Data/MeuDbContext.cs
@@ -8,6 +8,9 @@ namespace DevIO.Site.Data
 {
     public class MeuDbContext : DbContext
     {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
     }
 }
